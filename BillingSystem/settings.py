1202baaa19af829cb3f1ecb4bfe6cbff0011f6a4
EswarSaiBandi <eshwarsai.d@gmail.com>
2025-09-26 19:45:55 +0530

    addition of login page and making access based on role

--- BillingSystem/settings.py
+++ BillingSystem/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'BillingSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'billing_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234567890',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
